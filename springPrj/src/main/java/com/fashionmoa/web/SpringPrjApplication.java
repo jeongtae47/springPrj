e991678a1f53e36d8e6af5cbd29e958fed1beba1
jeongtae <jeongtae47@naver.com>
2020-07-10 16:20:34 +0900

    첫 프로젝트 커밋

---
+++ springPrj/src/main/java/com/fashionmoa/web/SpringPrjApplication.java
@@ -0,0 +1,13 @@
+package com.fashionmoa.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPrjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPrjApplication.class, args);
+	}
+
+}
